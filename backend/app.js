99a4f7de6c6a7772cc2d486677c94a05247c0689
Brin <brin.osebno@gmail.com>
2025-05-08 20:47:58 +0200

    ...

--- backend/app.js
+++ backend/app.js
@@ -60,4 +60,5 @@ app.use("/api/company/:companyId/employees", EmployeesRoutes);
 app.use("/api/company/:companyId/orders", OrderRoutes);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, "127.0.0.1", () => console.log(`Server running on port ${PORT}`));
+//server.listen(PORT, "127.0.0.1", () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
